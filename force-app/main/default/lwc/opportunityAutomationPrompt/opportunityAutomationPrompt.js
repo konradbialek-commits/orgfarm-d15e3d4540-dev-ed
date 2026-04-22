5af1f65d1cef37cf0ea01ee425b984731ce49cd1
Konrad Bialek <konrad.bialek@britenet.eu>
2026-04-22 11:41:45 +0200

    Add classes from previous project

--- force-app/main/default/lwc/opportunityAutomationPrompt/opportunityAutomationPrompt.js
+++ force-app/main/default/lwc/opportunityAutomationPrompt/opportunityAutomationPrompt.js
@@ -5,16 +5,13 @@ export default class OpportunityAutomationPrompt extends LightningElement {
     
     @api isToggled = false; 
     
-    // Salesforce automatically passes the available flow actions (Next, Finish, etc.) into this variable
     @api availableActions = []; 
 
-    // --- DYNAMIC BUTTON LOGIC ---
     get buttonLabel() {
         return this.isToggled ? 'Generate Records' : 'Close';
     }
 
     get buttonVariant() {
-        // 'brand' is the standard Salesforce blue button, 'neutral' is the standard white/gray button
         return this.isToggled ? 'brand' : 'neutral';
     }
 
@@ -22,7 +19,6 @@ export default class OpportunityAutomationPrompt extends LightningElement {
         return this.isToggled ? 'utility:magicwand' : 'utility:close';
     }
 
-    // --- EVENT HANDLERS ---
     handleToggleChange(event) {
         this.isToggled = event.target.checked;
         const attributeChangeEvent = new FlowAttributeChangeEvent('isToggled', this.isToggled);
@@ -30,12 +26,10 @@ export default class OpportunityAutomationPrompt extends LightningElement {
     }
 
     handleNext() {
-        // Check if NEXT is an available action in the flow, then fire the event
         if (this.availableActions.find((action) => action === 'NEXT')) {
             const navigateNextEvent = new FlowNavigationNextEvent();
             this.dispatchEvent(navigateNextEvent);
             
-        // If this is the very last screen of the flow, the action might be FINISH instead
         } else if (this.availableActions.find((action) => action === 'FINISH')) {
             const navigateFinishEvent = new FlowNavigationFinishEvent();
             this.dispatchEvent(navigateFinishEvent);
